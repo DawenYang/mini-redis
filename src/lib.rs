c63931d8c8619e4f6538785182e737e7b4f9c95b
Dawen <darwinyang0805@gmail.com>
2024-05-28 22:21:54 +1000

    add connection

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-pub mod frame;
-
+pub use connection::Connection;
 pub use frame::Frame;
 
+pub mod frame;
+
+pub mod connection;
+
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+pub type Result<T> = std::result::Result<T, Error>;
